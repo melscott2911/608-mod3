081ab9ecdb12d5da27f6ef469fc8aa43c798867f
melscott2911 <97756987+melscott2911@users.noreply.github.com>
2022-02-04 18:46:51 -0600

    Add files via upload
    Mod 3 Files 1-5

--- bult-in-min-max.py
+++ bult-in-min-max.py
@@ -1,1 +1,19 @@
+def maximum(value1, value2, value3):
+    """return the maximum of three values."""
+    max_value = value1
+    if value2 > max_value:
+        max_value = value2
+    if value3 > max_value:
+        max_value = value3
+    return max_value
 
+def minimum(value1,value2,value3,value4):
+    """return the minimum of three values."""
+    min_value= value1
+    if value2 < min_value:
+        min_value = value2
+    if value3 < min_value:
+        min_value= value3
+    if value4 < min_value:
+        min_value= value4
+    return min_value
---
+++ population-dispersion.py
@@ -0,0 +1,5 @@
+import statistics
+statistics.pvariance([1, 3, 4, 2, 6, 5, 3, 4, 5, 2])
+statistics.pstdev([1, 3, 4, 2, 6, 5, 3, 4, 5, 2])
+import math
+math.sqrt(statistics.pvariance([1, 3, 4, 2, 6, 5, 3, 4, 5, 2])
